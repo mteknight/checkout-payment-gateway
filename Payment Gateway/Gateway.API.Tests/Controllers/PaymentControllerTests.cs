d80328e45ab71bd24a5eeda0f9ffb93ce433a676
Alexandre Fernandes <mteknight@gmail.com>
2021-10-24 18:33:54 +0100

    added test to validate the proper result is returned

--- Payment Gateway/Gateway.API.Tests/Controllers/PaymentControllerTests.cs
+++ Payment Gateway/Gateway.API.Tests/Controllers/PaymentControllerTests.cs
@@ -53,6 +53,24 @@ namespace Gateway.API.Tests.Controllers
                 .Verify(service => service.Execute(It.IsAny<Payment>(), It.IsAny<CancellationToken>()), () => Times.Exactly(1));
         }
 
+        [Theory]
+        [AutoData]
+        public async Task GivenValidPayment_WhenRequestPaymentExecution_ThenReturnOkWithResult(
+            bool expectedResult,
+            Payment payment)
+        {
+            // Arrange
+            var sutController = this.GetSutController(expectedResult);
+
+            // Act
+            var result = await sutController.ExecutePayment(payment);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>("An Ok result is expected if the payment is processed.");
+            result.As<OkObjectResult>().Value.As<bool>().Should().Be(expectedResult, "The expected result should be returned.");
+
+        }
+
         private PaymentController GetSutController(bool expectedResult = default)
         {
             var mockedPaymentService = this.SetupPaymentServiceMock(expectedResult);

0b2053f937a32594f27558c1c6da5dc0d1abd14d
Alexandre Fernandes <mteknight@gmail.com>
2021-10-24 15:20:36 +0100

    added test for null service reference

---
+++ Payment Gateway/Gateway.Domain.Tests/Domain/PaymentTests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Gateway.Test.Domain
+{
+    public class PaymentTests
+    {
+        [Fact]
+        public Task GivenServiceNull_WhenExecutingPayment_ThenThrowArgumentNullException()
+        {
+            // Arrange
+            var payment = new Payment();
+            var service = default(IPaymentService);
+
+            // Act
+            async Task SutCall()
+            {
+                await payment.Execute(service);
+            }
+
+            Func<Task> sutCall = SutCall;
+
+            // Assert
+            return sutCall.Should().ThrowExactlyAsync<ArgumentNullException>("The service is required to perform the operation.");
+        }
+    }
+
+    public class Payment
+    {
+        public string CardNumber { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Currency { get; set; }
+
+        public string CVV { get; set; }
+
+        public Task<bool> Execute(IPaymentService service)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPaymentService
+    {
+        Task<bool> Execute(Payment payment);
+    }
+
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Execute(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
